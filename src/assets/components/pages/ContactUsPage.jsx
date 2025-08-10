a9f49e90c4225861b69f79e6f3bb562a6f271d04
AbdallahAhmed244 <167619706+AbdallahAhmed244@users.noreply.github.com>
2025-08-11 00:57:25 +0300

    Update ContactUsPage.jsx

--- src/assets/components/pages/ContactUsPage.jsx
+++ src/assets/components/pages/ContactUsPage.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import MainNavigationBar from "../layout/MainNavigationBar";
 import SiteFooter from "../layout/SiteFooter";
 import "../../styles/ContactUsPage.css";
